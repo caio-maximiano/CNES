c04e6ff083b5958ad0869658c00c235891294c2d
Caio Maximiano <caiomax97@hotmail.com>
2022-08-05 19:06:42 -0300

    commit 05/08

---
+++ incremental_load.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Definição das variáveis
+mes=$(date '+%Y%m%d')
+echo Baixando arquivos com data referência "$mes".
+
+#Baixando o arquivo .ZIP do servitor FTP no diretório de destino
+wget ftp://ftp.datasus.gov.br/cnes/BASE_DE_DADOS_CNES_$(date -d $mes +%Y%m).ZIP -P ZIP_FILES/$(date -d $mes +%Y%m)
+        
+#Extraindo somente arquivos necessários para um diretório temporário
+        unzip ZIP_FILES/$(date -d $start +%Y%m)/BASE_DE_DADOS_CNES_$(date -d $start +%Y%m).ZIP tbAtividadeProfissional$(date -d $start +%Y%m).csv -d CSV_FILES/$(date -d $start +%Y%m)
+        unzip ZIP_FILES/$(date -d $start +%Y%m)/BASE_DE_DADOS_CNES_$(date -d $start +%Y%m).ZIP tbCargaHorariaSus$(date -d $start +%Y%m).csv -d CSV_FILES/$(date -d $start +%Y%m)
+        unzip ZIP_FILES/$(date -d $start +%Y%m)/BASE_DE_DADOS_CNES_$(date -d $start +%Y%m).ZIP tbDadosProfissionalSus$(date -d $start +%Y%m).csv -d CSV_FILES/$(date -d $start +%Y%m)
+        unzip ZIP_FILES/$(date -d $start +%Y%m)/BASE_DE_DADOS_CNES_$(date -d $start +%Y%m).ZIP tbEstabelecimento$(date -d $start +%Y%m).csv -d CSV_FILES/$(date -d $start +%Y%m)
+        unzip ZIP_FILES/$(date -d $start +%Y%m)/BASE_DE_DADOS_CNES_$(date -d $start +%Y%m).ZIP rlEstabServClass$(date -d $start +%Y%m).csv -d CSV_FILES/$(date -d $start +%Y%m)
+        unzip ZIP_FILES/$(date -d $start +%Y%m)/BASE_DE_DADOS_CNES_$(date -d $start +%Y%m).ZIP tbMunicipio$(date -d $start +%Y%m).csv -d CSV_FILES/$(date -d $start +%Y%m) 
+        unzip ZIP_FILES/$(date -d $start +%Y%m)/BASE_DE_DADOS_CNES_$(date -d $start +%Y%m).ZIP tbClassificacaoServico$(date -d $start +%Y%m).csv -d CSV_FILES/$(date -d $start +%Y%m)
+        
+#Removendo arquivos zipados
+#rm  ZIP_FILES/$(date -d $mes +%Y%m)/BASE_DE_DADOS_CNES_$(date -d $mes +%Y%m).ZIP
+
+
+
+
